88fe97ef81002e076aecd95fa6c59db47f047bf5
timyu <timyu@truetel.com>
2018-11-04 21:45:24 -0800

    change mysql to h2

--- src/oam/schema.sql
+++
@@ -1,6 +0,0 @@
-create table user (
-	id integer not null auto_increment,
-	name varchar(128),
-	email varchar(128),
-	primary key (id)
-);
---
+++ src/oam/sql/mysql-init.sql
@@ -0,0 +1,6 @@
+create table user (
+       id integer not null auto_increment,
+       name varchar(128),
+       email varchar(128),
+       primary key (id)
+);
